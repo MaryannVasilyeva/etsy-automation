c7ced824d72fe3ddcf05ace8da2e928433e46d99
Maryann Vasilyeva <maryannvasilyeva@MacBook-Pro.local>
2022-05-06 16:22:11 -0700

    second commit

--- cypress/integration/1-getting-started/initialtest.spec.js
+++ cypress/integration/1-getting-started/initialtest.spec.js
@@ -1,8 +1,19 @@
 /// <reference types="cypress" />
 
 describe('Initial Test', () => {
-    it('visit website', ()=> {
+    before('visit website', ()=> {
         cy.visit('https://www.etsy.com/')
+    })
+    it('first test', ()=> {
+        //cy.get('.wt-pb-xs-1 wt-pb-lg-0 wt-pt-sm-1 wt-pt-lg-0 wt-pr-xs-0 wt-pr-sm-1 > span.wt-screen-reader-only').should('have.text', 'Etsy')
+        cy.get('input#global-enhancements-search-query').isTrue(true, 'this val is true')
+
+    })
+    it('second test', ()=> {
+        
+    })
+    it('third test', ()=> {
+        
     })
 })
 
